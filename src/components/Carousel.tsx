f0f1cecbdbad7ca6758ba5eb5949a25b1bfbba36
ety001 <work@akawa.ink>
2025-09-29 12:54:42 +0800

    carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 export default function Carousel() {
   return (
     <div className="carousel-vertical">
-      <figure className="figure-social box-shadow">
-        <div className="meta d-flex align-items-center">
+      <figure className="figure-social flex box-shadow">
+        <div className="meta flex items-center">
           <Image
-            className="img-fluid img-profile rounded-circle mr-3"
+            className="img-fluid img-profile rounded-full mr-3"
             src="/images/man-1.jpg"
             alt="User profile"
             width={40}
@@ -18,14 +18,14 @@ export default function Carousel() {
             <p className="m-0 small text-grey">Played on <Link href="https://steem-retrogames.com/" target="_blank" rel="noopener"><u>Steem Retrogames</u></Link></p>
           </div>
         </div>
-        <div className="bg-image logo d-block d-lg-none" style={{backgroundImage: 'url(/images/steem-retrogames-mobile.png)'}}></div>
-        <div className="bg-image logo d-none d-lg-block" style={{backgroundImage: 'url(/images/steem-retrogames.png)'}}></div>
+        <div className="bg-image logo block lg:hidden" style={{backgroundImage: 'url(/images/steem-retrogames-mobile.png)'}}></div>
+        <div className="bg-image logo hidden lg:block" style={{backgroundImage: 'url(/images/steem-retrogames.png)'}}></div>
       </figure>
       
-      <figure className="figure-social box-shadow">
-        <div className="meta d-flex align-items-center">
+      <figure className="figure-social flex box-shadow">
+        <div className="meta flex items-center">
           <Image
-            className="img-fluid img-profile rounded-circle mr-3"
+            className="img-fluid img-profile rounded-full mr-3"
             src="/images/girl-4.jpg"
             alt="User profile"
             width={40}
@@ -36,14 +36,14 @@ export default function Carousel() {
             <p className="m-0 small text-grey">Shared on <Link href="https://steematlas.com/" target="_blank" rel="noopener"><u>SteemAtlas</u></Link></p>
           </div>
         </div>
-        <div className="bg-image logo d-block d-lg-none" style={{backgroundImage: 'url(/images/steematlas-mobile.png)'}}></div>
-        <div className="bg-image logo d-none d-lg-block" style={{backgroundImage: 'url(/images/steematlas.png)'}}></div>
+        <div className="bg-image logo block lg:hidden" style={{backgroundImage: 'url(/images/steematlas-mobile.png)'}}></div>
+        <div className="bg-image logo hidden lg:block" style={{backgroundImage: 'url(/images/steematlas.png)'}}></div>
       </figure>
       
-      <figure className="figure-social box-shadow">
-        <div className="meta d-flex align-items-center">
+      <figure className="figure-social flex box-shadow">
+        <div className="meta flex items-center">
           <Image
-            className="img-fluid img-profile rounded-circle mr-3"
+            className="img-fluid img-profile rounded-full mr-3"
             src="/images/girl-1.jpg"
             alt="User profile"
             width={40}
@@ -54,14 +54,14 @@ export default function Carousel() {
             <p className="m-0 small text-grey">Posted on <Link href="https://steemit.com/" target="_blank" rel="noopener"><u>Steemit</u></Link></p>
           </div>
         </div>
-        <div className="bg-image logo d-block d-lg-none" style={{backgroundImage: 'url(/images/SteemIt-Mobile.jpg)'}}></div>
-        <div className="bg-image logo d-none d-lg-block" style={{backgroundImage: 'url(/images/SteemIt.jpg)'}}></div>
+        <div className="bg-image logo block lg:hidden" style={{backgroundImage: 'url(/images/SteemIt-Mobile.jpg)'}}></div>
+        <div className="bg-image logo hidden lg:block" style={{backgroundImage: 'url(/images/SteemIt.jpg)'}}></div>
       </figure>
       
-      <figure className="figure-social box-shadow">
-        <div className="meta d-flex align-items-center">
+      <figure className="figure-social flex box-shadow">
+        <div className="meta flex items-center">
           <Image
-            className="img-fluid img-profile rounded-circle mr-3"
+            className="img-fluid img-profile rounded-full mr-3"
             src="/images/h4lab-logo.jpeg"
             alt="User profile"
             width={40}
@@ -72,8 +72,8 @@ export default function Carousel() {
             <p className="m-0 small text-grey">Played on <Link href="https://hari-raid.h4lab.com/" target="_blank" rel="noopener"><u>HARI RAID</u></Link></p>
           </div>
         </div>
-        <div className="bg-image logo d-block d-lg-none" style={{backgroundImage: 'url(/images/hari-raid-logo-mobile.webp)'}}></div>
-        <div className="bg-image logo d-none d-lg-block" style={{backgroundImage: 'url(/images/hari-raid-logo.webp)'}}></div>
+        <div className="bg-image logo block lg:hidden" style={{backgroundImage: 'url(/images/hari-raid-logo-mobile.webp)'}}></div>
+        <div className="bg-image logo hidden lg:block" style={{backgroundImage: 'url(/images/hari-raid-logo.webp)'}}></div>
       </figure>
     </div>
   );
